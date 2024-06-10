6762af414e778b3b204957d38cae0f851971a7c8
Prasanth <jprasanth2006@gmail.com>
2024-06-10 14:28:40 +0530

    Update trusted-script.js

--- trusted-script.js
+++ trusted-script.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log('Trusted Script Executed');
+    let newElement = document.createElement("p");
+    newElement.textContent = "We are from HackerOne @jprasanth"
     document.body.innerHTML += '<p>We are from HackerOne</p>';
 })
